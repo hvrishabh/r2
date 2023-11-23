2075cb35207d81ec682744d9a61071c4fb5bcae5
hvrishabh <hitesh.verma@rishabhsoft.com>
2023-11-23 15:55:21 +0530

    according version 2 and till lect 92

--- 04-steps/src/90 Children PROPS.js
+++ 04-steps/src/90 Children PROPS.js
@@ -44,21 +44,10 @@ function Steps() {
             <div className={`${step >= 2 ? "active" : ""}`}> 2</div>
             <div className={`${step >= 3 ? "active" : ""}`}> 3</div>
           </div>
-
-          {/* <StepMessage step={step}>{messages[step - 1]}</StepMessage> */}
-
-          <StepMessage step={step}>
-            {messages[step - 1]}
-            <div className="buttons">
-              <Button
-                bgColor="#e7e7e7"
-                textColor="#333"
-                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
-              >
-                Learn How
-              </Button>
-            </div>
-          </StepMessage>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
 
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
@@ -74,14 +63,6 @@ function Steps() {
   );
 }
 
-function StepMessage({ step, children }) {
-  return (
-    <div className="message">
-      <h3>Step {step}</h3>
-      {children}
-    </div>
-  );
-}
 function Button({ textColor, bgColor, onClick, children }) {
   return (
     <button
